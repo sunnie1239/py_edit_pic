cf055df7c6caf6e67da6188c2b3b14a46c06fa4b
sunnie1239 <sunnie1239@gmail.com>
2020-04-21 22:47:18 -0700

    multi pics convert

--- edit.py
+++ edit.py
@@ -1,5 +1,8 @@
 from PIL import Image
+import os
 
-image_file = Image.open('FLOWERS.jpg')
-image_file = image_file.convert('L') # 轉為灰階
-image_file.save('result.png')
+for pic in os.listdir('.'):
+    if pic.endswith('.jpg'):
+        image_file = Image.open(pic)
+        image_file = image_file.convert('L') # 轉為灰階
+        image_file.save('res_'+ pic) 
