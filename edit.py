25d21d4a09af53481a71b8648c80e5440afb0131
sunnie1239 <sunnie1239@gmail.com>
2020-04-21 22:57:45 -0700

    save to documents

--- edit.py
+++ edit.py
@@ -1,8 +1,9 @@
 from PIL import Image
 import os
 
-for pic in os.listdir('.'):
+for pic in os.listdir('original'):
     if pic.endswith('.jpg'):
-        image_file = Image.open(pic)
+        image_file = Image.open('original/' + pic)
         image_file = image_file.convert('L') # 轉為灰階
-        image_file.save('res_'+ pic) 
+        # image_file.save('res_'+ pic) 
+        image_file.save('result/' + pic[:-4] + '_res.jpg') # 字串結合練習
