ce21323c8531eb6d831fdf7e3797f85a6c59537f
sunnie1239 <sunnie1239@gmail.com>
2020-04-21 23:08:30 -0700

    path join

--- edit.py
+++ edit.py
@@ -3,7 +3,6 @@ import os
 
 for pic in os.listdir('original'):
     if pic.endswith('.jpg'):
-        image_file = Image.open('original/' + pic)
+        image_file = Image.open(os.path.join('original', pic))
         image_file = image_file.convert('L') # 轉為灰階
-        # image_file.save('res_'+ pic) 
-        image_file.save('result/' + pic[:-4] + '_res.jpg') # 字串結合練習
+        image_file.save(os.path.join('result', pic[:-4] + '_res.jpg'))
